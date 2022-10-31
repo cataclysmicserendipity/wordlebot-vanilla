85ed1a56d3f517f49f6853c3aba772d8d362d25d
Jenn <jenniferdgaylord@gmail.com>
2022-10-31 16:35:03 -0500

    algorithm works on multiple rows

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ document.getElementById("rows").addEventListener("click", (e) => {
   }
 });
 
-document.getElementById("rowbutton").addEventListener("click", (e) => {
-  e.preventDefault();
+function makeRow() {
   let rows = document.getElementById("rows");
   let row = document.createElement("div");
   row.className = "row";
@@ -22,39 +21,37 @@ document.getElementById("rowbutton").addEventListener("click", (e) => {
     let box = document.createElement("input");
     box.className = "box invalid";
     box.type = "text";
+    box.dataset.column = i;
     row.appendChild(box);
   }
-
   rows.appendChild(row);
+}
+document.getElementById("rowbutton").addEventListener("click", (e) => {
+  e.preventDefault();
+  makeRow();
 });
+makeRow();
 
 let possible = [];
 
 document.getElementById("go").addEventListener("click", (e) => {
   e.preventDefault();
 
-  let box1 = document.getElementById("box1");
-  let box2 = document.getElementById("box2");
-  let box3 = document.getElementById("box3");
-  let box4 = document.getElementById("box4");
-  let box5 = document.getElementById("box5");
-
-  let boxArray = [box1, box2, box3, box4, box5];
+  const boxArray = document.getElementsByClassName("box");
 
   possible = words;
 
   for (let i = 0; i < boxArray.length; i++) {
     console.log(`testing box ${i}`);
     if (boxArray[i].classList.contains("valid")) {
-      console.log(`this box is valid`);
       let letter = boxArray[i].value.toLowerCase();
-      possible = possible.filter((word) => letter === word[i]);
+      let column = boxArray[i].dataset.column;
+      possible = possible.filter((word) => letter === word[column]);
     }
   }
   for (let i = 0; i < boxArray.length; i++) {
     if (boxArray[i].classList.contains("invalid")) {
       let letter = boxArray[i].value.toLowerCase();
-      console.log(`${i} is invalid`);
       for (let j = 0; j < possible.length; j++) {
         if (contains(possible[j], letter)) {
           possible.splice([j], 1);
@@ -72,11 +69,15 @@ document.getElementById("go").addEventListener("click", (e) => {
           j--;
         }
       }
+      let column = boxArray[i].dataset.column;
+      possible = possible.filter((word) => letter !== word[column]);
     }
   }
-  console.log(possible);
 
   let resultDiv = document.getElementById("possible");
+  while (resultDiv.firstChild) {
+    resultDiv.removeChild(resultDiv.firstChild);
+  }
   textNode = document.createTextNode(possible);
   resultDiv.appendChild(textNode);
 });
